3ab8f67e56497a42b1cafc04323089e78b97e3cc
tighug <kapuriko123@gmail.com>
2019-12-03 22:51:28 +0900

    [update] setup.shを拡充

--- setup.sh
+++ setup.sh
@@ -1,21 +1,33 @@
-#!/bin/bash
+#!/bin/bash -eu
 
-set -eu
+REPOS=(auction_db auction_api contract_api admin_web)
+BRANCHES=(master develop master develop)
+REPO_URL=https://gitlab.com/ylab-bc/2019/surplus-power-market/
+INIT_DIR=$PWD
 
-CLONE_TO=auction_db
-REPO_URL=https://gitlab.com/ylab-bc/2019/surplus-power-market/auction_db.git
-BRANCH=master
-
-if [ -d "$CLONE_TO" ]; then
-  cd $CLONE_TO
-  git fetch -p
-  git checkout -q $BRANCH
-  latest_rev=$(git ls-remote origin HEAD | awk '{print $1}')
-  current_rev=$(git rev-parse HEAD)
-  if [ "$latest_rev" != "$current_rev" ]; then
-    git reset --hard $(git log --pretty=format:%H | tail -1)
-    git pull
+for ((i = 0; i < 4; i++)); do
+  if [ -d "${REPOS[$i]}" ]; then
+    cd ${REPOS[$i]}
+    git fetch -p
+    git checkout -q ${BRANCHES[$i]}
+    latest_rev=$(git ls-remote origin HEAD | awk '{print $1}')
+    current_rev=$(git rev-parse HEAD)
+    if [ "$latest_rev" != "$current_rev" ]; then
+      git reset --hard $(git log --pretty=format:%H | tail -1)
+      git pull
+    fi
+  else
+    git clone "$REPO_URL${REPOS[$i]}.git"
+    cd ${REPOS[$i]}
   fi
-else
-  git clone $REPO_URL $CLONE_TO
-fi
+  echo ""
+
+  if [ -f package.json ]; then
+    yarn
+    echo ""
+  fi
+
+  cd $INIT_DIR
+done
+
+echo "Successfully Updated All Repository!"
