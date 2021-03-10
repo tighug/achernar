40e7d0cfcb0091c7c50ae7076f3e097d5c8e2bd0
tighug <kapuriko123@gmail.com>
2021-03-10 14:09:41 +0900

    feat: Add initial flow-db datas

--- flow-db/initdb.d/2_load.sql
+++ flow-db/initdb.d/2_load.sql
@@ -1,8 +1,4 @@
-USE flow;
-LOAD DATA LOCAL INFILE "/docker-entrypoint-initdb.d/feeders.csv" INTO TABLE feeders FIELDS TERMINATED BY "," ENCLOSED BY "" LINES TERMINATED BY "\n";
-LOAD DATA LOCAL INFILE "/docker-entrypoint-initdb.d/nodes.csv" INTO TABLE `nodes` FIELDS TERMINATED BY "," ENCLOSED BY "" LINES TERMINATED BY "\n";
-LOAD DATA LOCAL INFILE "/docker-entrypoint-initdb.d/lines.csv" INTO TABLE `lines` FIELDS TERMINATED BY "," ENCLOSED BY "" LINES TERMINATED BY "\n";
-LOAD DATA LOCAL INFILE "/docker-entrypoint-initdb.d/load_samples.csv" INTO TABLE load_samples FIELDS TERMINATED BY "," ENCLOSED BY "" LINES TERMINATED BY "\n";
-LOAD DATA LOCAL INFILE "/docker-entrypoint-initdb.d/pv_samples.csv" INTO TABLE pv_samples FIELDS TERMINATED BY "," ENCLOSED BY "" LINES TERMINATED BY "\n";
-LOAD DATA LOCAL INFILE "/docker-entrypoint-initdb.d/ehp_samples.csv" INTO TABLE ehp_samples FIELDS TERMINATED BY "," ENCLOSED BY "" LINES TERMINATED BY "\n";
-LOAD DATA LOCAL INFILE "/docker-entrypoint-initdb.d/uchp_samples.csv" INTO TABLE uchp_samples FIELDS TERMINATED BY "," ENCLOSED BY "" LINES TERMINATED BY "\n";
+LOAD DATA LOCAL INFILE "/docker-entrypoint-initdb.d/feeders.csv" INTO TABLE `Feeder` FIELDS TERMINATED BY "," ENCLOSED BY "" LINES TERMINATED BY "\n";
+LOAD DATA LOCAL INFILE "/docker-entrypoint-initdb.d/nodes.csv" INTO TABLE `Node` FIELDS TERMINATED BY "," ENCLOSED BY "" LINES TERMINATED BY "\n";
+LOAD DATA LOCAL INFILE "/docker-entrypoint-initdb.d/lines.csv" INTO TABLE `Line` FIELDS TERMINATED BY "," ENCLOSED BY "" LINES TERMINATED BY "\n";
+LOAD DATA LOCAL INFILE "/docker-entrypoint-initdb.d/samples.csv" INTO TABLE `Sample` FIELDS TERMINATED BY "," ENCLOSED BY "" LINES TERMINATED BY "\n";
